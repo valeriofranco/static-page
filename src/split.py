8498f1a0bffbdb19d63f7e71a79f5f9a721a331e
Valerio <valerio.franco2004@gmail.com>
2026-07-03 03:12:44 +0200

    added split.py

--- src/split.py
+++ src/split.py
@@ -6,37 +6,71 @@ def split_nodes_image(old_nodes: list[TextNode]) -> list[TextNode]:
     for node in old_nodes:
         if node.text == "":
             pass
-        print(node.text)
+        #print(node.text)
+        if extract_markdown_images(node.text) == []:
+            continue
         image_text = extract_markdown_images(node.text)
-        print(image_text)
-        print(image_text[0][0])
-        print(image_text[0][1])
-        print(image_text[1][0])
-        print(image_text[1][1])
-        split1 = node.text.split(f"![{image_text[0][0]}]({image_text[0][1]})")
-        split1 = split1[1]
-        print(split1.split(f"![{image_text[1][0]}]({image_text[1][1]})"))
-        middle_text = node.text.split(f"({image_text[0][1]})")
-        new_nodes.append()
-        #node.text.split(extract_markdown_images(node.text)[0])
+        
+        new_node = []
+        remaining_text = node.text
+        for i in range (0, len(image_text)):
+            temp_list = remaining_text.split(f"![{image_text[i][0]}]({image_text[i][1]})",1)
+            if len(temp_list) > 1:
+                remaining_text = temp_list[1]
+            if temp_list[0] == "":
+                continue
+            
+            new_node.append(temp_list[0])
+            new_node.append((image_text[i][0],image_text[i][1]))
+
+        for i in range(0,len(new_node)):
+            if not isinstance(new_node[i],tuple):     
+                new_nodes.append(TextNode(new_node[i],TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(new_node[i][0],TextType.IMAGE,new_node[i][1]))
+
     return new_nodes
         
 
-
 def split_nodes_link(old_nodes: list[TextNode]) -> list[TextNode]:
-    pass
+    new_nodes: list[TextNode] = []
+    for node in old_nodes:
+        if node.text == "":
+            pass
+
+        if extract_markdown_links(node.text) == []:
+            continue
+        link_text = extract_markdown_links(node.text)
+        
+        new_node = []
+        remaining_text = node.text
+        for i in range (0, len(link_text)):
+            temp_list = remaining_text.split(f"[{link_text[i][0]}]({link_text[i][1]})",1)
+            if len(temp_list) > 1:
+                remaining_text = temp_list[1]
+            if temp_list[0] == "":
+                continue
+            
+            new_node.append(temp_list[0])
+            new_node.append((link_text[i][0],link_text[i][1]))
+
+        for i in range(0,len(new_node)):
+            if not isinstance(new_node[i],tuple):     
+                new_nodes.append(TextNode(new_node[i],TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(new_node[i][0],TextType.LINK,new_node[i][1]))
+
+    return new_nodes
 
 
 
 
 
-
-
-node = TextNode(
-    "This is text with a link ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)",
+"""link_node = TextNode(
+    "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
     TextType.TEXT,
 )
-new_nodes = split_nodes_image([node])
+new_nodes = split_nodes_link([link_node])
 # [
 #     TextNode("This is text with a link ", TextType.TEXT),
 #     TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
@@ -45,3 +79,9 @@ new_nodes = split_nodes_image([node])
 #         "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
 #     ),
 # ]
+
+image_node = TextNode(
+    "This is text with a link ![to boot dev](https://www.boot.dev) and ![to youtube](https://www.youtube.com/@bootdotdev)",
+    TextType.TEXT,
+)
+new_nodes = split_nodes_image([image_node])"""
